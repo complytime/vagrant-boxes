13162c47c3f5d5e86f063e923c257fd3d4d4389f
Jay Flowers <jay.flowers@gmail.com>
2025-03-14 13:16:44 -0400

    add vars

--- arm64/provision/vbox-guest-additions.sh
+++ arm64/provision/vbox-guest-additions.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+VBOX_VERSION="7.1.6"
+
 sudo dnf install kernel-devel-$(uname -r) -y
 sudo dnf groupinstall "Development Tools" -y
 sudo dnf install dkms -y
 sudo dnf clean all
 
-wget http://download.virtualbox.org/virtualbox/7.1.6/VBoxGuestAdditions_7.1.6.iso
+wget "http://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso"
 sudo mkdir /media/VBoxGuestAdditions
-sudo mount -o loop,ro VBoxGuestAdditions_7.1.6.iso /media/VBoxGuestAdditions
+sudo mount -o loop,ro "VBoxGuestAdditions_${VBOX_VERSION}.iso" /media/VBoxGuestAdditions
 sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions-arm64.run
 
 sudo umount /media/VBoxGuestAdditions
 sudo rmdir /media/VBoxGuestAdditions
-rm VBoxGuestAdditions_7.1.6.iso
+rm "VBoxGuestAdditions_${VBOX_VERSION}.iso"
