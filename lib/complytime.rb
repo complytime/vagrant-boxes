8c5692c8775ea7a644c0afce9ba2c2e6845658b0
Jay Flowers <jay.flowers@gmail.com>
2025-03-18 09:01:26 -0400

    Improve vars

---
+++ lib/complytime.rb
@@ -0,0 +1,26 @@
+class ComplyTime
+  class << self
+    @@table = {}
+    @@logger = nil
+
+    def method_missing(method_symbol, *args, &block)
+      block_parameter = ", #{block.inspect}" if block_given?
+      Vagrant.global_logger.debug "
+      method_missing:
+        #{caller[0]}:#{method_symbol}(#{args.inspect}#{block_parameter})
+      "
+
+      if !args[0].nil?
+        @@table[method_symbol] = args[0]
+      end
+      @@table[method_symbol]
+    end
+
+    def load()
+      file_path = '../vagrant-vars.rb'
+      self.class_eval(File.read(file_path), file_path, 1)
+    end
+  end
+end
+
+ComplyTime.load
