a7f18abdde5fce96cc0019e90a290049244c8461
Jay Flowers <jay.flowers@gmail.com>
2025-04-02 11:48:30 -0400

    switch to official centos stream vagrant box for amd64

--- bin/vagrant-build.bash
+++ bin/vagrant-build.bash
@@ -2,10 +2,12 @@
 
 source "$(dirname ${BASH_SOURCE[0]})/../lib/control/bootstrap.bash"
 
-export BOX_FILE="$(absolute_path package.box)"
+if [[ $ARCH != "amd64" ]]; then
+  export BOX_FILE="$(absolute_path package.box)"
 
-[[ -f "${BOX_FILE}" ]]
-fail_if "Package file doesn't exist: ${BOX_FILE}"
+  [[ -f "${BOX_FILE}" ]]
+  fail_if "Package file doesn't exist: ${BOX_FILE}"
+fi
 
 cd "${ARCH}"
 
