f88f1693412386bb18b46195b87bb81f15ef68e4
Jay Flowers <jay.flowers@gmail.com>
2025-04-02 17:36:33 -0400

    fix vagant build script, box file and able to rerun

--- bin/vagrant-build.bash
+++ bin/vagrant-build.bash
@@ -2,13 +2,15 @@
 
 source "$(dirname ${BASH_SOURCE[0]})/../lib/control/bootstrap.bash"
 
+cd "${ARCH}"
+
 if [[ $ARCH != "amd64" ]]; then
-  export BOX_FILE="$(absolute_path package.box)"
+  export BOX_FILE="$(absolute_path centos-stream-10.box)"
 
   [[ -f "${BOX_FILE}" ]]
   fail_if "Package file doesn't exist: ${BOX_FILE}"
 fi
 
-cd "${ARCH}"
+vagrant up --no-provision
 
-vagrant up
+vagrant provision
