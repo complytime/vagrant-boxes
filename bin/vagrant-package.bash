859763bd128734237599283978fa316ae3c4cdda
Jay Flowers <jay.flowers@gmail.com>
2025-04-03 14:58:56 -0400

    the vbox provider halt was replacing generic ssh key, this fixes that

--- bin/vagrant-package.bash
+++ bin/vagrant-package.bash
@@ -18,7 +18,26 @@ Looks like you haven't run make vagrant-build"
 vagrant up --no-provision
 
 vagrant ssh -c "$(cat ${PATHS_PROJECT_HOME}/provision/vagrant-ssh.sh)"
+rm .vagrant/machines/default/virtualbox/private_key
+fail_if "failed to delete the private_key: .vagrant/machines/default/virtualbox/private_key"
 
-vagrant halt
+#vagrant halt
+
+if [[ $ARCH == "arm64" ]]; then
+  good "VBoxManage controlvm ${machine_id} shutdown"
+  VBoxManage controlvm "${machine_id}" shutdown
+  fail_if "error shutting machine down"
+
+  while [[ true ]]; do
+    running_vms="$(VBoxManage list runningvms)"
+    fail_if "error collecting running vms"
+
+    debug "${running_vms}"
+    if echo "${running_vms}" | grep -v "${machine_id}" ; then
+      good "vm is shutdown ${machine_id}"
+      break;
+    fi
+  done
+fi
 
 vagrant package --base $machine_id --output "${BOX_FILE}"
